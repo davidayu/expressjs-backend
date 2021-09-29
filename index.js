d4ecbc6d9925bad4d6fbbf53075a0ffb4f84ebf9
davidayu <50340109+davidayu@users.noreply.github.com>
2021-09-28 22:04:03 -0700

    Added new API functionality for get, post, delete

--- index.js
+++ index.js
@@ -40,8 +40,82 @@ app.get('/', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    const job = req.query.job;
+    if (name != undefined) {
+        if (job != undefined) {
+            let result = findUserByNameAndJob(name, job);
+            result = {users_list: result};
+            res.send(result);
+        } else {
+            let result = findUserByName(name);
+            result = {users_list: result};
+            res.send(result);
+        }
+
+    } else if (job != undefined) {
+        let result = findUserByJob(job);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else {
+        res.send(users);
+    }
 });
+
+const findUserByName = (name) => { 
+    return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+const findUserByJob = (job) => { 
+    return users['users_list'].filter( (user) => user['job'] === job); 
+}
+
+const findUserByNameAndJob = (name, job) => { 
+    return users['users_list'].filter( (user) => user['name'] === name && user['job'] === job); 
+}
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.send(result);
+    }
+});
+
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id); // or line below
+    //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
+
+function addUser(user){
+    users['users_list'].push(user);
+}
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id'];
+    let user = findUserById(id);
+    if (user === undefined || user.length == 0)
+        res.status(404).send('User not found.');
+    else {
+        removeUser(user);
+        res.status(200).end();
+    }
+});
+
+function removeUser(user){
+    const index = users['users_list'].indexOf(user);
+    users['users_list'].splice(index, 1);
+}
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
