7dc83383a0e55dad222fa40ec8ecf12a9b5363d3
davidayu <50340109+davidayu@users.noreply.github.com>
2021-09-29 23:23:05 -0700

    Added FE support for id

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function findUserById(id) {
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
-    res.status(201).end();
+    res.status(201).send(userToAdd);
 });
 
 function addUser(user){
